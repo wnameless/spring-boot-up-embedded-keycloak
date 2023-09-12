0510c6a5ebd6c1c0ff3c6ff9d81b5602cdb8b648
wmw <wnameless@gmail.com>
2023-09-12 19:41:46 +0800

    Add spring-boot-up-core

--- src/main/java/com/github/wnameless/spring/boot/up/embedded/keycloak/config/KeycloakServerPropertiesHolder.java
+++ src/main/java/com/github/wnameless/spring/boot/up/embedded/keycloak/config/KeycloakServerPropertiesHolder.java
@@ -3,8 +3,11 @@ package com.github.wnameless.spring.boot.up.embedded.keycloak.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
+import com.github.wnameless.spring.boot.up.SpringBootUpApplicationContextProvider;
 
+@DependsOn(SpringBootUpApplicationContextProvider.BEAN_NAME)
 @ConditionalOnBean(annotation = {Configuration.class, EnableEmbeddedKeycloak.class})
 @Component(KeycloakServerPropertiesHolder.BEAN_NAME)
 public class KeycloakServerPropertiesHolder {
